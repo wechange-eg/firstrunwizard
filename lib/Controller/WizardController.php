1d091ebb8d3c100c789ab7bd088a089a9c908926
Sascha Narr <saschanarr@gmail.com>
2022-09-23 15:52:18 +0200

    Reapply changes for WECHANGE

--- lib/Controller/WizardController.php
+++ lib/Controller/WizardController.php
@@ -62,7 +62,7 @@ class WizardController extends Controller {
 		$this->theming = $theming;
 		$this->groupManager = $groupManager;
 
-		$this->slides = explode(',', $this->config->getAppValue(Application::APP_ID, 'slides', 'video,values,apps,clients,final'));
+		$this->slides = explode(',', $this->config->getAppValue(Application::APP_ID, 'slides', 'content,video,values,apps,clients,final'));
 	}
 
 	/**
@@ -93,12 +93,15 @@ class WizardController extends Controller {
 
 		$slides = [];
 
-		$slides[] = $this->staticSlide('page.values', $data);
+		/* wechange overwrites*/
+        $slides[] = $this->staticSlide('page.content', $data);
+        /*
 		if ($appStore && $this->groupManager->isAdmin($this->userId)) {
 			$slides[] = $this->staticSlide('page.apps', $data);
 		}
 		$slides[] = $this->staticSlide('page.clients', $data);
 		$slides[] = $this->staticSlide('page.final', $data);
+		*/
 
 		return new JSONResponse([
 			'hasVideo' => in_array('video', $this->slides, true),
